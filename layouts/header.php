dd76ebfaa2e86c9f43a1147fe6ddffe4cab46c73
mahmoudsiliman <mahmoudsiliman75@gmail.com>
2021-01-04 16:17:12 +0200

    add arabic style

--- layouts/header.php
+++ layouts/header.php
@@ -17,6 +17,7 @@
 	<!-- BEGIN: CSS Assets-->
 	<link rel="stylesheet" href="dist/css/app.css" />
 	<link rel="stylesheet" href="dist/css/custom-style.css" />
+	<!-- <link rel="stylesheet" href="dist/css/app-ar.css" /> -->
 	<link rel="stylesheet" href="dist/css/print.css" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
   <!-- END: CSS Assets-->
@@ -258,12 +259,12 @@
 				</ul>
 			</li>
 
-			<!-- <li>
+			<li>
 				<a href="financial.php" class="side-menu">
 					<div class="side-menu__icon"> <i class="ml-3" data-feather="dollar-sign"></i> </div>
-					<div class="side-menu__title"> Financilas </div>
+					<div class="side-menu__title"> Vizetta </div>
 				</a>
-			</li> -->
+			</li>
 
 		</ul>
 	</nav>
@@ -313,81 +314,8 @@
 		</div>
 		<!-- END: SYSTEM THEMES -->
 
-		<!-- BEGIN: Notifications -->
-		<div class="intro-x dropdown relative mr-auto  ml-2 sm:mr-6">
-				<div class="dropdown-toggle notification notification--bullet cursor-pointer"> <i data-feather="bell" class="notification__icon dark:text-gray-300"></i> </div>
-				<div class="notification-content dropdown-box mt-8 absolute top-0 left-0 sm:left-auto sm:right-0 z-20 -ml-10 sm:ml-0">
-						<div class="notification-content__box dropdown-box__content box dark:bg-dark-6">
-								<div class="notification-content__title">Notifications</div>
-								<div class="cursor-pointer relative flex items-center ">
-										<div class="w-12 h-12 flex-none image-fit mr-1">
-												<img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-13.jpg">
-												<div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-										</div>
-										<div class="ml-2 overflow-hidden">
-												<div class="flex items-center">
-														<a href="javascript:;" class="font-medium truncate mr-5">Angelina Jolie</a> 
-														<div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">06:05 AM</div>
-												</div>
-												<div class="w-full truncate text-gray-600">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#039;s standard dummy text ever since the 1500</div>
-										</div>
-								</div>
-								<div class="cursor-pointer relative flex items-center mt-5">
-										<div class="w-12 h-12 flex-none image-fit mr-1">
-												<img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-2.jpg">
-												<div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-										</div>
-										<div class="ml-2 overflow-hidden">
-												<div class="flex items-center">
-														<a href="javascript:;" class="font-medium truncate mr-5">Kevin Spacey</a> 
-														<div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">01:10 PM</div>
-												</div>
-												<div class="w-full truncate text-gray-600">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomi</div>
-										</div>
-								</div>
-								<div class="cursor-pointer relative flex items-center mt-5">
-										<div class="w-12 h-12 flex-none image-fit mr-1">
-												<img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-5.jpg">
-												<div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-										</div>
-										<div class="ml-2 overflow-hidden">
-												<div class="flex items-center">
-														<a href="javascript:;" class="font-medium truncate mr-5">Leonardo DiCaprio</a> 
-														<div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">01:10 PM</div>
-												</div>
-												<div class="w-full truncate text-gray-600">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomi</div>
-										</div>
-								</div>
-								<div class="cursor-pointer relative flex items-center mt-5">
-										<div class="w-12 h-12 flex-none image-fit mr-1">
-												<img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-10.jpg">
-												<div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-										</div>
-										<div class="ml-2 overflow-hidden">
-												<div class="flex items-center">
-														<a href="javascript:;" class="font-medium truncate mr-5">Edward Norton</a> 
-														<div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">01:10 PM</div>
-												</div>
-												<div class="w-full truncate text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem </div>
-										</div>
-								</div>
-								<div class="cursor-pointer relative flex items-center mt-5">
-										<div class="w-12 h-12 flex-none image-fit mr-1">
-												<img alt="Midone Tailwind HTML Admin Template" class="rounded-full" src="dist/images/profile-8.jpg">
-												<div class="w-3 h-3 bg-theme-9 absolute right-0 bottom-0 rounded-full border-2 border-white"></div>
-										</div>
-										<div class="ml-2 overflow-hidden">
-												<div class="flex items-center">
-														<a href="javascript:;" class="font-medium truncate mr-5">Johnny Depp</a> 
-														<div class="text-xs text-gray-500 ml-auto whitespace-no-wrap">01:10 PM</div>
-												</div>
-												<div class="w-full truncate text-gray-600">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem </div>
-										</div>
-								</div>
-						</div>
-				</div>
-		</div>
-		<!-- END: Notifications -->
+    <!-- START:: SWITCH LANGUAGE BUTTON -->
+    <!-- END:: SWITCH LANGUAGE BUTTON -->
 
 		<!-- BEGIN: Account Menu -->
 		<div class="intro-x dropdown w-8 h-8 relative">
